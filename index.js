205152698bc4dd8bd2bc0cde3f4859db08796bb6
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-09-27 16:21:24 +0330

    fix(index.js): remove supabase configure

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-import { createClient } from "@supabase/supabase-js";
-
 //import your components
-
- const supabase = createClient(process.env.UNITY_URL, process.env.UNITY_API_KEY);
-
-
-
-
-
- export {
-
-    supabase
- }
