53ec57a2939d7701ec0398215da740200d0e6b98
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-09-30 15:27:01 +0330

    fix(upload): get data

--- app/upload/page.js
+++ app/upload/page.js
@@ -1,39 +1,57 @@
-"use client"
-import { uuidv4 } from "uuid";
-import {supabase} from "../../lib/supabase"
-import { useState } from "react"
+"use client";
+import { v4 as  uuidv4 } from "uuid";
+import { supabase } from "../../lib/supabase";
+import { useState } from "react";
+import Image from "next/image";
 
 export default function upload() {
+  const [file, setFile] = useState([]);
+  const [im, setImg] = useState([]);
 
-    const [file,setFile] = useState([])
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const fileName = `${file.name}`;
+    // ${uuidv4()}-
+    
 
-    const handleSubmit = async(e)=>{
-        e.preventDefault();
-        const fileName = `${uuidv4()}-${file.name}`
+    const { data, error } = await supabase.storage
+      .from("unity-image")
+      .upload(fileName, file, {
+        cacheControl: "3600",
+        upsert: false,
+      });
 
-        const {data, error} = await supabase.storage
-        .from("unity-image")
-        .upload(fileName, file, {
-            cacheControl : "3600",
-            upsert : false ,
-
-        });
-
-        const filePath = data.path
-    }
-
-    const handleFileSelected = (e) =>{
-            setFile(e.target.files[0])
-    }
-
-  return (
-   <form onSubmit={handleSubmit}>
-
-    <input type="file" name="image" onChange={handleFileSelected}/>
-    <button type="submit"> Upload image</button>
+    const filePath = data.path;
+        console.log(filePath);
 
 
+    const { data: data2 } = await supabase.storage
+      .from("unity-image")
+      .getPublicUrl(`${filePath}`);
 
-   </form>
-  )
+     setImg(data2)
+      
+  };
+
+const handleFileSelected = (e) => {
+  setFile(e.target.files[0]);
+};
+
+// const read = async () => {
+//   const { data } = await supabase.storage
+//     .from("unity-image")
+//     .getPublicUrl(`${filePath}`);
+// };
+// let x = read();
+// console.log(x);
+
+return (
+  <>
+    <form onSubmit={handleSubmit}>
+      <input type="file" name="image" onChange={handleFileSelected} />
+      <button type="submit"> Upload image</button>
+    </form>
+    <Image src={im}/>
+  </>
+);
 }
