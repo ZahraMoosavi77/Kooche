e9d88e1b022401e607666336ceecf40df5ed7ae2
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-10-21 17:29:10 +0330

    add(SubtitleNewPage)

--- app/new/page.jsx
+++ app/new/page.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Optional from '@/components/elements/Optional'
 import RegisterAd from '@/components/elements/RegisterAd'
 import TitleNewPage from '@/components/elements/TitleNewPage'
+import SubTitleNewPage from '@/components/elements/SubTitleNewPage'
 import { infoGame } from '@/constants/constantNewPage'
 import { infoSale } from '@/constants/constantNewPage'
 import { infoSeller } from '@/constants/constantNewPage'
@@ -12,6 +13,10 @@ export default function page() {
     <TitleNewPage text={infoGame.INFOGAMETITLE}/>
     <TitleNewPage text={infoSeller.INFOSELLERTITLE}/>
     <TitleNewPage text={infoSale.INFOSALETITLE}/>
+    <SubTitleNewPage text={infoGame.INFOGAMESUBTITLE}/>
+    <SubTitleNewPage text={infoSeller.INFOSELLERSUBTITLE}/>
+    <SubTitleNewPage text={infoSale.INFOSALESUBTITLE}/>
+
     <Optional/>
     <Optional/>
     </div>
