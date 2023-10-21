8819e2fa2393cd05550d0b696a72f07c561b75ec
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-10-21 10:39:55 +0330

    add component

--- app/new/page.jsx
+++ app/new/page.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Optional from '@/components/elements/Optional'
 import RegisterAd from '@/components/elements/RegisterAd'
+import TitleNewPage from '@/components/elements/TitleNewPage'
+import { infoGame } from '@/constants/constantNewPage'
 
 export default function page() {
   return (
     <div className='flex flex-col justify-center items-center p-12'>
     <RegisterAd/>
+    <TitleNewPage text={infoGame.INFOGAMETITLE}/>
     <Optional/>
     <Optional/>
     </div>
