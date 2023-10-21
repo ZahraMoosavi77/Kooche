04f90535d33d5e4c2e335ebd8075b9d43eeb0f53
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-10-21 10:38:42 +0330

    add(titleNewPage): component

---
+++ components/elements/TitleNewPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TitleNewPage({text}) {
+  return (
+    <div className='text-scales-body font-bold leading-leading-2 text-gray-900 text-right'>{text}</div>
+  )
+}
