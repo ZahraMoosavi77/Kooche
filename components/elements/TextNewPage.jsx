dd29f88dfadea9d0cfe6bd46601d721cd7f9531e
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-10-21 18:53:21 +0330

    add TextNewPage comonent

---
+++ components/elements/TextNewPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TextNewPage({text}) {
+  return (
+    <div className='text-scales-default font-peyda-regular leading-leading-3 text-gray-900'>{text}</div>
+  )
+}
