f08cea0e5724e01c8691ed002a21ab8b17f5a7b4
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-10-21 09:49:08 +0330

    add registerAd component to newpage

--- app/new/page.jsx
+++ app/new/page.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Optional from '@/components/elements/Optional'
+import RegisterAd from '@/components/elements/RegisterAd'
 
 export default function page() {
   return (
-    <>
+    <div className='flex flex-col justify-center items-center p-12'>
+    <RegisterAd/>
     <Optional/>
-    </>
+    <Optional/>
+    </div>
   )
 }
