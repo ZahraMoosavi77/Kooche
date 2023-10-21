4e303019018b4f9fff20da422e666bd519e4189c
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-10-21 10:39:23 +0330

    fix style

--- components/elements/RegisterAd.jsx
+++ components/elements/RegisterAd.jsx
@@ -2,6 +2,6 @@ import { REGISTERADVERTISE } from '@/constants/constantNewPage'
 
 export default function RegisterAd() {
   return (
-    <div className='text-scales-xlarge font-bold' >{REGISTERADVERTISE }</div>
+    <div className='text-scales-xlarge font-bold leading-leading-5 text-right' >{REGISTERADVERTISE }</div>
   )
 }
