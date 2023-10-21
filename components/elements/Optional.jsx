3312079f362b1d30cd06a4982a8d0b59b40dbf69
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-10-21 18:52:01 +0330

    add style

--- components/elements/Optional.jsx
+++ components/elements/Optional.jsx
@@ -3,6 +3,6 @@ import { OPTIONAL } from "@/constants/constantNewPage"
 
 export default function Optional() {
   return (
-    <div className="text-scales-caption text-gray-600 capitalize text-right">{OPTIONAL}</div>
+    <div className="text-scales-caption text-gray-600 capitalize text-right font-peyda-regular pt-1 pr-1">{OPTIONAL}</div>
   )
 }
