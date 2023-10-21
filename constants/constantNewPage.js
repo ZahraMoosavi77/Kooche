3a22aa792e098de65d57bc97967d6860ad7dcdd4
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-10-21 08:26:10 +0330

    add(constantNewPage.js)

---
+++ constants/constantNewPage.js
@@ -0,0 +1,31 @@
+import { useMemo } from "react";
+
+export const OPTIONAL = "(اختیاری)";
+export const CANCEL = "انصراف";
+export const REGISTERADVERTISE = "ثبت آگهی";
+export const infoGame = {
+  INFOGAMETITLE: "اطلاعات بازی",
+  INFOGAMESUBTITLE: "مشخصات بازی که قصد تبادل یا فروش آن را دارید وارد کنید.",
+  GAMENAME: "اسم بازی",
+  GAMECONSOLE: "کنسول بازی",
+  GAMEDESCRIPTION: "توضیحات",
+  GAMEIMAGES: "تصاویر بازی",
+  GAMEIMAGE: "تصویر بازی",
+};
+
+export const infoSeller = {
+  INFOSELLERTITLE: "اطلاعات فروشنده",
+  INFOSELLERSUBTITLE: "اطلاعات فردی که فروشنده این بازی هست را وارد کنید.",
+  SELLERNAME: "اسم  فروشنده",
+  SELLERCITY: "شهر",
+  SELLERPHONE: "شماره تماس",
+};
+
+export const infoSale = {
+  INFOSALETITLE: "مشخصات فروش",
+  INFOSALESUBTITLE: "در این بخش مشخص کنید مایل به فروش، تبادل یا هر دو مورد هستید.",
+  SALEGAME: "فروش بازی",
+  PRICEGAME: "قیمت بازی",
+  EXCHANGEGAME: "تبادل با بازی های دیگر",
+  PREFEREDEXCHANGEGAME: "بازی هایی که باآنها تعویض میکنم",
+};
