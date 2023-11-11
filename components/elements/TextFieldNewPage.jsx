b9bc1204233731d1ce64ee02f31c84e8cb10d745
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-11-11 18:41:03 +0330

    fix(TextFieldNewPage): deleted extra codes

--- components/elements/TextFieldNewPage.jsx
+++ components/elements/TextFieldNewPage.jsx
@@ -1,10 +1,7 @@
 'use client'
 import { NewContext } from "@/context/NewPageContext"
-import { useContext, useEffect, useState } from "react"
-import { REGex } from "@/constants/constantNewPage"
-import { validateConfig } from "next/dist/server/config-shared";
+import { useContext} from "react"
 import TextError from "./TextError";
-import { ERRORPHONENUMBER } from "@/constants/constantNewPage";
 import TextHelper from '../elements/TextHelper'
 import { TEXTHELPER } from '@/constants/constantNewPage'
 
@@ -17,8 +14,6 @@ export default function TextField({ type, label, name, errormessage, helpText, v
       <input id={name} type={type} onChange={onChange} value={value} name={name} className={` ${!validate ? 'bg-accent-error' : 'bg-gray-200'}  text-gray-900 rounded-xl  outline-none px-3 py-[9.5px] w-full mt-1 focus:bg-white focus:border focus:border-primary focus:text-primary`} />
       {!validate ? <TextError text={errormessage} /> : ''}
       {helpText && <TextHelper specialClass={'pr-3'} text={TEXTHELPER} />}
-
-
     </>
 
 
