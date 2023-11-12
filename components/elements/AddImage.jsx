f03252d42aeb445c5d6fd1a07d2b2d8fe2464cfa
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-11-12 09:40:05 +0330

    fix(AddImage): fixed bordercolor

--- components/elements/AddImage.jsx
+++ components/elements/AddImage.jsx
@@ -6,9 +6,10 @@ import addPlus from '../../asset/icons/AddPlus.svg'
 export default function AddImage() {
   return (
    
-          <div className=' flex flex-col justify-center items-center border-1 border-primary-100 rounded-xl border-solid w-28 h-28'>
+          <div className=' flex flex-col justify-center items-center border border-primary-100 rounded-xl border-solid w-28 h-28'>
            <Image src={addPlus} width={40} height={40} alt="Add-Plus"/>
-          <TextNewPage text={infoGame.GAMEIMAGE}/></div>
+          <TextNewPage text={infoGame.GAMEIMAGE}/>
+          </div>
 
           
   
