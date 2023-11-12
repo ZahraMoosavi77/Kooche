1ced4324a821ea7e75c87057b56c84516608b1b4
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-11-12 10:54:07 +0330

    responsive new page

--- components/elements/RegisterAdButton.jsx
+++ components/elements/RegisterAdButton.jsx
@@ -6,7 +6,7 @@ import { NewContext } from "@/context/NewPageContext"
 import { REGex } from "@/constants/constantNewPage"
 export default function RegisterAdButton({ text }) {
   const { isValidName, isValidSellerName, isValidPrice, isValidPhoneNumber,isValidCity, setIsValidCity,isValidProvince, setIsValidProvince, values, setIsValidName, setIsValidPhoneNumber, setIsValidSellerName, setIsValidPrice } = useContext(NewContext);
-  const { name, price, preferedExchange, moreInfo, platformId, cityId, provinceId } = values;
+  const { name, price, preferedExchange, moreInfo, platformId, cityId, provinceId , exchange} = values;
 
   const handleInsertData = async () => {
     if (!values.name.trim()) setIsValidName(false)
@@ -16,21 +16,23 @@ export default function RegisterAdButton({ text }) {
     if (!values.platformId) setIsValidProvince(false);
     if (!values.cityId) setIsValidCity(false);
 
-
+     
     if (isValidName && isValidPrice && isValidPhoneNumber && isValidSellerName) {
       const { data, error } = await supabase
         .from('games')
         .insert([
 
-          { name: name, platformId: platformId || null, moreInfo: moreInfo || null, price: price || null, cityId: cityId, provinceId: provinceId, preferedExchange: preferedExchange || null },
+          { name: name, platformId: platformId || null, moreInfo: moreInfo || null, price: price || null, cityId: cityId, provinceId: provinceId, preferedExchange: preferedExchange || null , exchange:exchange || null },
         ])
         .select()
+
+        if(error) console.log('try again');
     }
 
 
   }
   return (
-
-    <button onClick={handleInsertData} className='font-peyda-medium text-white bg-primary leading-leading-3 rounded-xl px-4 py-2 w-[184px] h-12 hover:bg-primary-600'>{text}</button>
+    // w-[184px]
+    <button onClick={handleInsertData} className='font-peyda-medium text-white bg-primary leading-leading-3 rounded-xl px-4 py-2 w-[72%] sm:w-[184px]  h-12 hover:bg-primary-600'>{text}</button>
   )
 }
