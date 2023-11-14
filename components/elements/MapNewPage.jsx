5e0a518c8c6507a535aa9c0018abc694a4592837
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-11-14 15:02:41 +0330

    fix(newpage): fixed zindex

--- components/elements/MapNewPage.jsx
+++ components/elements/MapNewPage.jsx
@@ -2,10 +2,14 @@ import { MapContainer,TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 export default function MapNewPage() {
-  const CENTER = [35.68804331563681, 51.38883302970867];
+  const mapOptions = {
+    center: [35.68804331563681, 51.38883302970867],
+    zoom: 10,
+    scrollWheelZoom: true,
+  };
   return (
     <div className='rounded-xl w-full h-[300px]'>
-      <MapContainer center={CENTER} zoom={10} scrollWheelZoom={true} className="w-full h-full">
+      <MapContainer {...mapOptions} className="w-full h-full">
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       </MapContainer>
     </div>
