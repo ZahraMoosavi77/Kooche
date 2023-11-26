423603366486b7dede5fd28d7b68bd07760ae590
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-11-26 11:43:51 +0330

    fix(InfoGameSection): deleted optional word

--- components/modules/InfoGameSection.jsx
+++ components/modules/InfoGameSection.jsx
@@ -2,7 +2,7 @@
 import AddImage from '@/components/elements/new/AddImage'
 import TextFieldNewPage from '@/components/elements/new/TextFieldNewPage'
 import SubTitleNewPage from '@/components/elements/new/SubTitleNewPage'
-import {  infoGame } from '@/constants/constantNewPage'
+import { infoGame } from '@/constants/constantNewPage'
 import TitleNewPage from '@/components/elements/new/TitleNewPage'
 import Optional from '@/components/elements/new/Optional'
 import TextNewPage from '@/components/elements/new/TextNewPage'
@@ -12,42 +12,41 @@ import { CHOOSECONSOLE } from '@/constants/constantNewPage'
 import { SEARCHCONSOLE } from '@/constants/constantNewPage'
 import { useContext, useEffect, useState } from 'react'
 import { supabase } from '@/lib/supabase'
-import {ErrorMessageNameOfGame} from '@/constants/constantNewPage'
+import { ErrorMessageNameOfGame } from '@/constants/constantNewPage'
 import { NewContext } from "@/context/NewContext"
 
 
 
 export default function InfoGameSection() {
-    const [platforms , setPlatforms] = useState([]);
-    const { values,isValidName } = useContext(NewContext)
-    const getData= async () =>{
+    const [platforms, setPlatforms] = useState([]);
+    const { values, isValidName } = useContext(NewContext)
+    const getData = async () => {
         let { data, error } = await supabase
             .from('platforms')
             .select('*')
-         setPlatforms(data);
+        setPlatforms(data);
     }
-    useEffect( () => {
+    useEffect(() => {
         getData()
     }, [])
     return (
-    
+
         <div className=' flex flex-col gap-4 '>
             <div className=' flex flex-col gap-2'>
                 <TitleNewPage text={infoGame.INFOGAMETITLE} />
                 <SubTitleNewPage text={infoGame.INFOGAMESUBTITLE} />
             </div>
             <div>
-                <TextFieldNewPage value={values.name} validate={isValidName}  required={true} errormessage={ErrorMessageNameOfGame} helpText={true} name={'name'} type={'text'} label={<TextNewPage specialClass={'pr-3'} text={infoGame.GAMENAME} />} />
+                <TextFieldNewPage value={values.name} validate={isValidName} required={true} errormessage={ErrorMessageNameOfGame} helpText={true} name={'name'} type={'text'} label={<TextNewPage specialClass={'pr-3'} text={infoGame.GAMENAME} />} />
             </div>
 
             <div className='relative'>
                 <SelectOptionsNewPage column={'platformId'} validate={true} defualtValue={CHOOSECONSOLE} placeholderSearch={SEARCHCONSOLE} optionsGroup={platforms} label={<div className='flex  '>
                     <TextNewPage specialClass={'pr-3'} text={infoGame.GAMECONSOLE} />
-                    <Optional />
                 </div>} />
             </div>
             <div >
-                <TextAreaNewPage  name={'moreInfo'} value={values.moreInfo} label={<div className='flex  '>
+                <TextAreaNewPage name={'moreInfo'} value={values.moreInfo} label={<div className='flex  '>
                     <TextNewPage specialClass={'pr-3'} text={infoGame.GAMEDESCRIPTION} type={'textarea'} />
                     <Optional />
                 </div>} />
